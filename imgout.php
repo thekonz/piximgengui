e75e77a489d06ad1f30d79117d01d7bfe9434417
Konstantin Zinnen <zinnenk@gmail.com>
2013-08-25 19:47:46 +0200

    namespaces

--- imgout.php
+++ imgout.php
@@ -1,5 +1,5 @@
 <?php
 require_once '../piximgen/PixImGen.php';
-$imggen = new PixImGen($_POST);
+$imggen = new \thekonz\PixImGen($_POST);
 header('content-type: image/png');
 echo base64_encode($imggen->getImage());
