73dc4d58c32e90caf8847404e7e960f50b25bd28
Konstantin Zinnen <zinnenk@gmail.com>
2013-08-25 18:04:25 +0200

    final, without composer

--- imgout.php
+++ imgout.php
@@ -1,9 +1,5 @@
 <?php
 require_once '../piximgen/PixImGen.php';
 $imggen = new PixImGen($_POST);
-try {
-	header('content-type: image/png');
-	echo base64_encode($imggen->getImage());
-} catch(Exception $e) {
-	echo $e->getMessage();
-}
+header('content-type: image/png');
+echo base64_encode($imggen->getImage());
