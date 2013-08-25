2d5a346718cb6b99c8bfab3946154696f56ecb39
Konstantin Zinnen <zinnenk@gmail.com>
2013-08-25 15:24:05 +0200

    test imgout

--- imgout.php
+++ imgout.php
@@ -1,6 +1,10 @@
 <?php
 require_once '../piximgen/PixImGen.php';
 $imggen = new PixImGen($_POST);
-//header('content-type: image/png');
-echo $imggen->getImage();
-// print_r($imggen->settings);
+try {
+	//header('content-type: image/png');
+	echo $imggen->getImage();
+	// print_r($imggen->settings);
+} catch(Exception $e) {
+	echo $e->getMessage();
+}
