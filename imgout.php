bc8570fb42e3700abbf6d21457059541493bff4d
Konstantin Zinnen <zinnenk@gmail.com>
2013-08-25 14:57:55 +0200

    fix imgout

--- imgout.php
+++ imgout.php
@@ -3,4 +3,4 @@ require_once '../piximgen/PixImGen.php';
 $imggen = new PixImGen($_POST);
 // header('content-type: image/png');
 // echo base64_encode($imggen->getImage());
-echo $imggen->settings;
+print_r($imggen->settings);
