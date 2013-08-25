f282a7a6a512d22936bd374a9b874c318c1a7e1b
Konstantin Zinnen <zinnenk@gmail.com>
2013-08-25 20:48:54 +0200

    composer stuff

--- imgout.php
+++ imgout.php
@@ -1,5 +1,9 @@
 <?php
+/**
+ * - Inlude composer autoloader
+ * - Create a new instance of the PixImGen and set the settings
+ *  - Echo the Image base64 encoded
+ */
 require_once 'vendor/autoload.php';
 $imggen = new \thekonz\PixImGen($_POST);
-header('content-type: image/png');
 echo base64_encode($imggen->getImage());
