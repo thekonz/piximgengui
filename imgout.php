8e1cd983e948ac2189026011cd85f7c001c685c3
Konstantin Zinnen <zinnenk@gmail.com>
2013-08-25 14:59:31 +0200

    gitignore

--- imgout.php
+++ imgout.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../piximgen/PixImGen.php';
 $imggen = new PixImGen($_POST);
-// header('content-type: image/png');
-// echo base64_encode($imggen->getImage());
-print_r($imggen->settings);
+header('content-type: image/png');
+echo base64_encode($imggen->getImage());
+// print_r($imggen->settings);
