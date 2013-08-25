a315e58e2a6fff91bf7d5124e6f3a3d4794e8be7
Konstantin Zinnen <zinnenk@gmail.com>
2013-08-25 15:36:48 +0200

    base64 imgout

--- imgout.php
+++ imgout.php
@@ -3,7 +3,7 @@ require_once '../piximgen/PixImGen.php';
 $imggen = new PixImGen($_POST);
 try {
 	header('content-type: image/png');
-	echo $imggen->getImage();
+	echo base64_encode($imggen->getImage());
 } catch(Exception $e) {
 	echo $e->getMessage();
 }
